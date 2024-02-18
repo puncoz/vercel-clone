6717e4a852ba4a01e700782f0e000fe47d1ac537
Puncoz Nepal <puncoz138@gmail.com>
2024-02-19 03:03:05 +0900

    s3 reverse proxy

---
+++ s3-reverse-proxy/index.js
@@ -0,0 +1,34 @@
+"use strict"
+
+const express = require("express")
+const httpProxy = require("http-proxy")
+
+const app = express()
+const PORT = 8000
+
+const BUCKET_NAME = "vercel-clone"
+const REGION = "ap-south-1"
+const BASE_PATH = `https://${BUCKET_NAME}.s3.${REGION}.amazonaws.com/__sources`
+
+const proxy = httpProxy.createProxy()
+
+app.use((req, res) => {
+    const hostname = req.hostname
+    const subdomain = hostname.split(".")[0]
+
+    const resolvesTo = `${BASE_PATH}/${subdomain}`
+
+    return proxy.web(req, res, {
+        target: resolvesTo,
+        changeOrigin: true,
+    })
+})
+
+proxy.on("proxyReq", (proxyReq, req, res) => {
+    const url = req.url
+    if (url === "/") {
+        proxyReq.path += "index.html"
+    }
+})
+
+app.listen(PORT, () => console.log(`Reverse proxy running on port ${PORT}`))
