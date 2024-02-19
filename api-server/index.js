0b3b1716f7ab6438575207b08d95295f829866cf
Puncoz Nepal <puncoz138@gmail.com>
2024-02-19 13:18:23 +0900

    api server

---
+++ api-server/index.js
@@ -0,0 +1,70 @@
+"use strict"
+
+const express = require("express")
+const { generateSlug } = require("random-word-slugs")
+const { ECSClient, RunTaskCommand } = require("@aws-sdk/client-ecs")
+
+const app = express()
+const PORT = 9000
+
+const ecsClient = new ECSClient({
+    region: "ap-south-1",
+    credentials: {
+        accessKeyId: "",
+        secretAccessKey: "",
+    },
+})
+
+const config = {
+    CLUSTER_ARN: "arn:aws:ecs:ap-south-1:000000000000:cluster/vercel-clone",
+    TASK_ARN: "arn:aws:ecs:ap-south-1:000000000000:task-definition/vercel-clone-builder-task",
+}
+
+app.post("/project", async (req, res) => {
+    const { gitUrl } = req.body
+    const projectSlug = generateSlug()
+
+    // spin the container
+    const command = new RunTaskCommand({
+        cluster: config.CLUSTER_ARN,
+        taskDefinition: config.TASK_ARN,
+        launchType: "FARGATE",
+        count: 1,
+        networkConfiguration: {
+            awsvpcConfiguration: {
+                subnets: ["subnet-00000000", "subnet-00000001", "subnet-00000002"],
+                securityGroups: "sg-00000000",
+                assignPublicIp: "ENABLED",
+            },
+        },
+        overrides: {
+            containerOverrides: [
+                {
+                    name: "vercel-clone-builder-image",
+                    environment: [
+                        {
+                            name: "GIT_REPOSITORY",
+                            value: gitUrl,
+                        },
+                        {
+                            name: "PROJECT_ID",
+                            value: projectSlug,
+                        },
+                    ],
+                },
+            ],
+        },
+    })
+
+    await ecsClient.send(command)
+
+    return res.json({
+        status: "queued",
+        data: {
+            projectSlug,
+            url: `http://${projectSlug}.localhost:8000`,
+        },
+    })
+})
+
+app.listen(PORT, () => console.log(`Api server listening on port ${PORT}`))
